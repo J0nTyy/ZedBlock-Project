15356d69f34de9dadc1becd6b6f4766a40c9823a
Kartikey Sharma <kartikey7547js@gmail.com>
2023-12-19 07:41:11 +0530

    Created signup and login logic in views.py Introduced intentional bug

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 def login(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        password = request.POST.get('upassword')
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/home')
+        else:
+            redirect('/login')
     return render(request, 'blog_app/login.html')
 
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        email = request.POST.get('uemail')
+        password = request.POST.get('upassword')
+        new_user = User.objects.create_user(username=name, email=email, password=password)
+        new_user.save()
+        return redirect('/login')
     return render(request, 'blog_app/signup.html')
 
 
